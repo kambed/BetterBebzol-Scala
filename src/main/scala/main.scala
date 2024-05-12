7842d1fb99b0374c6100b9f8250706e856b24d34
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2024-05-12 22:35:13 +0200

    Fix merge

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,6 +2,7 @@ import akka.actor.typed.ActorSystem
 import akka.http.scaladsl.Http
 import model.command.abstracts.Command
 import rest.api.RestRoutes
+import util.Supervisor
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.util.{Failure, Success}
