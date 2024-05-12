897a14e21bf1d4fbdad9a18ea9071a853b467e21
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2024-05-12 15:53:42 +0200

    Validate requests

--- src/main/scala/rest/api/RestRoutes.scala
+++ src/main/scala/rest/api/RestRoutes.scala
@@ -3,10 +3,12 @@ package rest.api
 import akka.http.scaladsl.server.Directives.*
 import akka.http.scaladsl.server.Route
 import rest.api.controller.{CreateProductController, ListAllProductsController}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpResponse, StatusCodes}
+import akka.http.scaladsl.server.{Directives, RejectionHandler}
 
 class RestRoutes {
 
-  val allRoutes: Route =
+  val allRoutes: Route = Route.seal(
     pathPrefix("api") {
       pathPrefix("v1") {
         productRoutes
@@ -16,9 +18,19 @@ class RestRoutes {
         }
       }
     }
+  )
 
   private lazy val productRoutes: Route = path("product") {
     ListAllProductsController() ~
       CreateProductController()
   }
+
+  implicit def rejectionHandler: RejectionHandler =
+    RejectionHandler.default
+      .mapRejectionResponse {
+        case res@HttpResponse(_, _, ent: HttpEntity.Strict, _) =>
+          val message = ent.data.utf8String.replaceAll("\"", "\'").replaceAll("\n", " ").replaceAll(" {2}", " ")
+          res.withEntity(HttpEntity(ContentTypes.`application/json`, s"""{"rejection": "$message"}"""))
+        case x => x
+      }
 }
