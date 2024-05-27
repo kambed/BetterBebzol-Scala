7bc24badbdf71b80a569c354ceb5840ca961e451
Damian Biskupski <ddamian2057@gmail.com>
2024-05-27 19:01:01 +0200

    moving host and port as configuration variables

--- build.sbt
+++ build.sbt
@@ -20,6 +20,7 @@ val SwaggerJaxrs2JakartaVersion = "2.2.21"
 val JacksonVersion = "2.17.1"
 val MySqlConnectorVersion = "8.0.33"
 val SlickVersion = "3.5.1"
+val ConfigVersion = "0.6.1"
 libraryDependencies ++= Seq(
   "com.typesafe.scala-logging" %% "scala-logging" % ScalaLoggingVersion,
   "ch.qos.logback" % "logback-classic" % LogbackVersion,
@@ -34,5 +35,6 @@ libraryDependencies ++= Seq(
   "mysql" % "mysql-connector-java" % MySqlConnectorVersion,
   "com.typesafe.slick" %% "slick" % SlickVersion,
   "com.github.jwt-scala" %% "jwt-core" % "10.0.1",
-  "at.favre.lib" % "bcrypt" % "0.10.2"
+  "at.favre.lib" % "bcrypt" % "0.10.2",
+  "com.github.kxbmap" %% "configs" % ConfigVersion
 )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,6 @@
 import akka.actor.typed.ActorSystem
 import akka.http.scaladsl.Http
+import com.typesafe.config.{Config, ConfigFactory}
 import model.command.abstracts.Command
 import rest.api.RestRoutes
 import util.Supervisor
@@ -10,14 +11,17 @@ import scala.util.{Failure, Success}
 object Main extends App {
   implicit val system: ActorSystem[Command] = ActorSystem[Command](Supervisor(), "system")
   implicit val executionContext: ExecutionContextExecutor = system.executionContext
+  implicit val config: Config = ConfigFactory.load.resolve
+  private val serverHost: String = config.getString("akka.http.server.host")
+  private val serverPort: Int = config.getInt("akka.http.server.port")
 
-  system.log.info("Starting webserver...")
-  private val bindingFuture = Http().newServerAt("localhost", 8080).bindFlow(new RestRoutes().allRoutes)
+  system.log.info(s"Starting webserver on $serverHost:$serverPort...")
+  private val bindingFuture = Http().newServerAt(serverHost, serverPort).bindFlow(new RestRoutes().allRoutes)
   bindingFuture.onComplete {
     case Success(_) =>
       system.log.info("==========================================================")
-      system.log.info("| Webserver started on http://localhost:8080             |")
-      system.log.info("| Swagger UI available on http://localhost:8080/api-docs |")
+      system.log.info(s"| Webserver started on http://$serverHost:$serverPort             |")
+      system.log.info(s"| Swagger UI available on http://$serverHost:$serverPort/api-docs |")
       system.log.info("==========================================================")
     case Failure(exception) =>
       system.log.error("Failed to start webserver", exception)
